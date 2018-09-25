0a2caa7e6062066e0674f8456ffa5656f6b80249
Khanh Dao <khanhdao.knd@gmail.com>
2018-09-25 13:02:33 +0700

    Explicit from address to send transaction

--- test/NFTokenUnitTests/approveUnitTest.js
+++ test/NFTokenUnitTests/approveUnitTest.js
@@ -9,7 +9,7 @@ contract('NFToken: approve Unit Test', function (accounts) {
   const RECIPIENT_URL = 'recipient url'
   const RECIPIENT_ADDRESS = '0x2'
 
-  const NFT_OWNER = accounts[0] // by default, accounts[0] is the paying address for all transactions
+  const NFT_OWNER = accounts[0]
   const NFT_ID = 1
   const NFT_TRAITS = '0x01'
   const NFT_TYPE = '0x01'
@@ -21,7 +21,7 @@ contract('NFToken: approve Unit Test', function (accounts) {
 
   let springNFTInstance
 
-  beforeEach(async function () {
+  beforeEach(async () => {
     springNFTInstance = await springNFT.new(SIGNER_ADDRESS, MANAGER_ADDRESS)
 
     await springNFTInstance.addRecipient(
@@ -30,28 +30,28 @@ contract('NFToken: approve Unit Test', function (accounts) {
       NFT_ID, NFT_OWNER, RECIPIENT_ID, NFT_TRAITS, NFT_TYPE, { from: SIGNER_ADDRESS })
   })
 
-  it('checks that proper values are updated', async function () {
+  it('checks that proper values are updated', async () => {
     await springNFTInstance.approve(NFT_APPROVED_ADDRESS, NFT_ID)
 
     const actualAddress = await springNFTInstance.getApproved.call(NFT_ID)
     assert.equal(actualAddress, NFT_APPROVED_ADDRESS)
   })
 
-  it('throw if msg.sender doesnt have permission to approve', async function () {
+  it('throw if msg.sender doesnt have permission to approve', async () => {
     await utils.assertRevert(
       springNFTInstance.approve(NFT_APPROVED_ADDRESS, NFT_ID, { from: MANAGER_ADDRESS }))
   })
 
-  it('throw if owner doesnt exist for tokenId', async function () {
+  it('throw if owner doesnt exist for tokenId', async () => {
     await utils.assertRevert(springNFTInstance.approve(NFT_APPROVED_ADDRESS, NON_EXISTENT_NFT_ID))
   })
 
-  it('throw if approved address is token owner', async function () {
-    await utils.assertRevert(springNFTInstance.approve(NFT_OWNER, NFT_ID))
+  it('throw if approved address is token owner', async () => {
+    await utils.assertRevert(springNFTInstance.approve(NFT_OWNER, NFT_ID, { from: NFT_OWNER }))
   })
 
-  it('checks that approval event is emitted', async function () {
-    const res = await springNFTInstance.approve(NFT_APPROVED_ADDRESS, NFT_ID)
+  it('checks that approval event is emitted', async () => {
+    const res = await springNFTInstance.approve(NFT_APPROVED_ADDRESS, NFT_ID, { from: NFT_OWNER })
     assert.equal(res.logs[0].event, 'Approval')
   })
 })
