66607905adfa8c03fc2bb897e03fa0c0f559541f
Khanh Dao <khanhdao.knd@gmail.com>
2018-09-27 13:39:42 +0700

    Extract test data

--- test/NFTokenUnitTests/balanceOfUnitTest.js
+++ test/NFTokenUnitTests/balanceOfUnitTest.js
@@ -1,26 +1,29 @@
 'use strict'
 
+const {
+  NFT_A_ID,
+  NFT_A_TRAITS,
+  NFT_A_TYPE,
+  NFT_B_ID,
+  NFT_B_TRAITS,
+  NFT_B_TYPE,
+  NON_EXISTENT_ADDRESS,
+  RAINFOREST,
+  RAINFOREST_TRUST_ADDRESS,
+  RAINFOREST_TRUST_ID,
+  RAINFOREST_TRUST_URL
+} = require('../test-data')
+
 const assert = require('chai').assert
 const springNFT = artifacts.require('SpringNFT.sol')
 const utils = require('../utils/utils')
 
 contract('NFToken: balanceOf Unit Test', (accounts) => {
-  const RECIPIENT_ID = '0x1'
-  const RECIPIENT_NAME = 'recipient name'
-  const RECIPIENT_URL = 'recipient url'
-  const RECIPIENT_ADDRESS = '0x2'
-
-  const NFT_OWNER = accounts[0]
-  const NFT_ID = 1
-  const NFT_TRAITS = '0x01'
-  const NFT_TYPE = '0x01'
-  const TRANSFERING_NFT_ID = 2
-
   const MANAGER_ADDRESS = accounts[7]
   const SIGNER_ADDRESS = accounts[6]
-  const TRANSFERED_TOKEN_ADDRESS = accounts[3]
-  const TRANSFERING_TOKEN_ADDRESS = accounts[2]
-  const NON_EXISTENT_ADDRESS = '0x0'
+  const OWNER_ADDRESS = accounts[0]
+  const TRANSFERRED_ADDRESS = accounts[3]
+  const TRANSFERRING_ADDRESS = accounts[2]
 
   let springNFTInstance
 
@@ -28,33 +31,35 @@ contract('NFToken: balanceOf Unit Test', (accounts) => {
     springNFTInstance = await springNFT.new(SIGNER_ADDRESS, MANAGER_ADDRESS)
 
     await springNFTInstance.addRecipient(
-      RECIPIENT_ID, RECIPIENT_NAME, RECIPIENT_URL, RECIPIENT_ADDRESS, { from: SIGNER_ADDRESS })
+      RAINFOREST_TRUST_ID, RAINFOREST, RAINFOREST_TRUST_URL, RAINFOREST_TRUST_ADDRESS,
+      { from: SIGNER_ADDRESS })
     await springNFTInstance.createNFT(
-      NFT_ID, NFT_OWNER, RECIPIENT_ID, NFT_TRAITS, NFT_TYPE, { from: SIGNER_ADDRESS })
+      NFT_A_ID, OWNER_ADDRESS, RAINFOREST_TRUST_ID, NFT_A_TRAITS, NFT_A_TYPE,
+      { from: SIGNER_ADDRESS })
     await springNFTInstance.createNFT(
-      TRANSFERING_NFT_ID, TRANSFERING_TOKEN_ADDRESS, RECIPIENT_ID, NFT_TRAITS, NFT_TYPE,
+      NFT_B_ID, TRANSFERRING_ADDRESS, RAINFOREST_TRUST_ID, NFT_B_TRAITS, NFT_B_TYPE,
       { from: SIGNER_ADDRESS }
     )
   })
 
   it('checks owner balance having 1 token', async () => {
-    assert.equal(await springNFTInstance.balanceOf.call(NFT_OWNER), 1)
+    assert.equal(await springNFTInstance.balanceOf.call(OWNER_ADDRESS), 1)
   })
 
   it('checks owner balance having 0 token when transfering 1 token to new address',
     async () => {
-      await springNFTInstance.transferFrom(NFT_OWNER, TRANSFERED_TOKEN_ADDRESS, NFT_ID,
-        { from: NFT_OWNER })
+      await springNFTInstance.transferFrom(OWNER_ADDRESS, TRANSFERRED_ADDRESS, NFT_A_ID,
+        { from: OWNER_ADDRESS })
 
-      assert.equal(await springNFTInstance.balanceOf(NFT_OWNER), 0)
+      assert.equal(await springNFTInstance.balanceOf(OWNER_ADDRESS), 0)
     })
 
   it('checks owner balance having 2 tokens when being transferred 1 token from new address',
     async () => {
-      await springNFTInstance.transferFrom(TRANSFERING_TOKEN_ADDRESS, NFT_OWNER, TRANSFERING_NFT_ID,
-        { from: TRANSFERING_TOKEN_ADDRESS })
+      await springNFTInstance.transferFrom(TRANSFERRING_ADDRESS, OWNER_ADDRESS, NFT_B_ID,
+        { from: TRANSFERRING_ADDRESS })
 
-      assert.equal(await springNFTInstance.balanceOf(NFT_OWNER), 2)
+      assert.equal(await springNFTInstance.balanceOf(OWNER_ADDRESS), 2)
     })
 
   it('throws if zero address is given', async () => {
