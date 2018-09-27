70be0e7d690cc47bf555026b0b0f155c3b6964cf
Khanh Dao <khanhdao.knd@gmail.com>
2018-09-27 13:42:52 +0700

    Extract test data (#16)

--- test/NFTokenUnitTests/approveUnitTest.js
+++ test/NFTokenUnitTests/approveUnitTest.js
@@ -1,24 +1,25 @@
 'use strict'
 
+const {
+  NFT_A_ID,
+  NFT_A_TRAITS,
+  NFT_A_TYPE,
+  NON_EXISTENT_NFT_ID,
+  RAINFOREST,
+  RAINFOREST_TRUST_ADDRESS,
+  RAINFOREST_TRUST_ID,
+  RAINFOREST_TRUST_URL
+} = require('../test-data')
+
 const assert = require('chai').assert
 const springNFT = artifacts.require('SpringNFT.sol')
 const utils = require('../utils/utils')
 
 contract('NFToken: approve Unit Test', (accounts) => {
-  const RECIPIENT_ID = '0x1'
-  const RECIPIENT_NAME = 'recipient name'
-  const RECIPIENT_URL = 'recipient url'
-  const RECIPIENT_ADDRESS = '0x2'
-
-  const NFT_OWNER = accounts[0]
-  const NFT_ID = 1
-  const NFT_TRAITS = '0x01'
-  const NFT_TYPE = '0x01'
-  const NON_EXISTENT_NFT_ID = 2
-
   const MANAGER_ADDRESS = accounts[7]
   const SIGNER_ADDRESS = accounts[6]
-  const NFT_APPROVED_ADDRESS = accounts[2]
+  const OWNER_ADDRESS = accounts[0]
+  const APPROVED_ADDRESS = accounts[2]
 
   let springNFTInstance
 
@@ -26,33 +27,36 @@ contract('NFToken: approve Unit Test', (accounts) => {
     springNFTInstance = await springNFT.new(SIGNER_ADDRESS, MANAGER_ADDRESS)
 
     await springNFTInstance.addRecipient(
-      RECIPIENT_ID, RECIPIENT_NAME, RECIPIENT_URL, RECIPIENT_ADDRESS, { from: SIGNER_ADDRESS })
+      RAINFOREST_TRUST_ID, RAINFOREST, RAINFOREST_TRUST_URL, RAINFOREST_TRUST_ADDRESS,
+      { from: SIGNER_ADDRESS })
     await springNFTInstance.createNFT(
-      NFT_ID, NFT_OWNER, RECIPIENT_ID, NFT_TRAITS, NFT_TYPE, { from: SIGNER_ADDRESS })
+      NFT_A_ID, OWNER_ADDRESS, RAINFOREST_TRUST_ID, NFT_A_TRAITS, NFT_A_TYPE,
+      { from: SIGNER_ADDRESS })
   })
 
   it('checks that proper values are updated', async () => {
-    await springNFTInstance.approve(NFT_APPROVED_ADDRESS, NFT_ID)
+    await springNFTInstance.approve(APPROVED_ADDRESS, NFT_A_ID)
 
-    const actualAddress = await springNFTInstance.getApproved.call(NFT_ID)
-    assert.equal(actualAddress, NFT_APPROVED_ADDRESS)
+    const actualAddress = await springNFTInstance.getApproved.call(NFT_A_ID)
+    assert.equal(actualAddress, APPROVED_ADDRESS)
   })
 
   it('throw if msg.sender doesnt have permission to approve', async () => {
     await utils.assertRevert(
-      springNFTInstance.approve(NFT_APPROVED_ADDRESS, NFT_ID, { from: MANAGER_ADDRESS }))
+      springNFTInstance.approve(APPROVED_ADDRESS, NFT_A_ID, { from: MANAGER_ADDRESS }))
   })
 
   it('throw if owner doesnt exist for tokenId', async () => {
-    await utils.assertRevert(springNFTInstance.approve(NFT_APPROVED_ADDRESS, NON_EXISTENT_NFT_ID))
+    await utils.assertRevert(springNFTInstance.approve(APPROVED_ADDRESS, NON_EXISTENT_NFT_ID))
   })
 
   it('throw if approved address is token owner', async () => {
-    await utils.assertRevert(springNFTInstance.approve(NFT_OWNER, NFT_ID, { from: NFT_OWNER }))
+    await utils.assertRevert(
+      springNFTInstance.approve(OWNER_ADDRESS, NFT_A_ID, { from: OWNER_ADDRESS }))
   })
 
   it('checks that approval event is emitted', async () => {
-    const res = await springNFTInstance.approve(NFT_APPROVED_ADDRESS, NFT_ID, { from: NFT_OWNER })
+    const res = await springNFTInstance.approve(APPROVED_ADDRESS, NFT_A_ID, { from: OWNER_ADDRESS })
     assert.equal(res.logs[0].event, 'Approval')
   })
 })
--- test/NFTokenUnitTests/getOwnedTokenListUnitTest.js
+++ test/NFTokenUnitTests/getOwnedTokenListUnitTest.js
@@ -1,26 +1,26 @@
 'use strict'
 
+const {
+  NFT_A_ID,
+  NFT_A_TRAITS,
+  NFT_A_TYPE,
+  NFT_B_ID,
+  NFT_B_TRAITS,
+  NFT_B_TYPE,
+  RAINFOREST,
+  RAINFOREST_TRUST_ADDRESS,
+  RAINFOREST_TRUST_ID,
+  RAINFOREST_TRUST_URL
+} = require('../test-data')
+
 const assert = require('chai').assert
 const springNFT = artifacts.require('SpringNFT.sol')
 
 contract('NFToken: approve Unit Test', (accounts) => {
-  const RECIPIENT_ID = '0x1'
-  const RECIPIENT_NAME = 'recipient name'
-  const RECIPIENT_URL = 'recipient url'
-  const RECIPIENT_ADDRESS = '0x2'
-
-  const NFT_OWNER = accounts[0]
-  const FIRST_NFT_ID = 1
-  const FIRST_NFT_TRAITS = '0x01'
-  const FIRST_NFT_TYPE = '0x01'
-
-  const SECOND_NFT_ID = 3
-  const SECOND_NFT_TRAITS = '0x03'
-  const SECOND_NFT_TYPE = '0x03'
-
   const MANAGER_ADDRESS = accounts[7]
   const SIGNER_ADDRESS = accounts[6]
-  const TRANSFERED_ACCOUNT_ADDRESS = accounts[2]
+  const OWNER_ADDRESS = accounts[0]
+  const TRANSFERRED_ADDRESS = accounts[2]
 
   let springNFTInstance
 
@@ -28,29 +28,30 @@ contract('NFToken: approve Unit Test', (accounts) => {
     springNFTInstance = await springNFT.new(SIGNER_ADDRESS, MANAGER_ADDRESS)
 
     await springNFTInstance.addRecipient(
-      RECIPIENT_ID, RECIPIENT_NAME, RECIPIENT_URL, RECIPIENT_ADDRESS, { from: SIGNER_ADDRESS })
-    await springNFTInstance.createNFT(
-      FIRST_NFT_ID, NFT_OWNER, RECIPIENT_ID, FIRST_NFT_TRAITS, FIRST_NFT_TYPE,
+      RAINFOREST_TRUST_ID, RAINFOREST, RAINFOREST_TRUST_URL, RAINFOREST_TRUST_ADDRESS,
       { from: SIGNER_ADDRESS })
     await springNFTInstance.createNFT(
-      SECOND_NFT_ID, NFT_OWNER, RECIPIENT_ID, SECOND_NFT_TRAITS, SECOND_NFT_TYPE,
+      NFT_A_ID, OWNER_ADDRESS, RAINFOREST_TRUST_ID, NFT_A_TRAITS, NFT_A_TYPE,
+      { from: SIGNER_ADDRESS })
+    await springNFTInstance.createNFT(
+      NFT_B_ID, OWNER_ADDRESS, RAINFOREST_TRUST_ID, NFT_B_TRAITS, NFT_B_TYPE,
       { from: SIGNER_ADDRESS })
   })
 
   it('checks that owner has 2 tokens by creation', async () => {
-    const tokenList = await springNFTInstance.getOwnedTokenList(NFT_OWNER)
+    const tokenList = await springNFTInstance.getOwnedTokenList(OWNER_ADDRESS)
 
     assert.lengthOf(tokenList, 2)
-    assert.equal(tokenList[0], FIRST_NFT_ID)
-    assert.equal(tokenList[1], SECOND_NFT_ID)
+    assert.equal(tokenList[0], NFT_A_ID)
+    assert.equal(tokenList[1], NFT_B_ID)
   })
 
   it('checks that owner has 1 token left after transfering to another account', async () => {
-    await springNFTInstance.transferFrom(NFT_OWNER, TRANSFERED_ACCOUNT_ADDRESS, FIRST_NFT_ID)
+    await springNFTInstance.transferFrom(OWNER_ADDRESS, TRANSFERRED_ADDRESS, NFT_A_ID)
 
-    const tokenList = await springNFTInstance.getOwnedTokenList(NFT_OWNER)
+    const tokenList = await springNFTInstance.getOwnedTokenList(OWNER_ADDRESS)
 
     assert.lengthOf(tokenList, 1)
-    assert.equal(tokenList[0], SECOND_NFT_ID)
+    assert.equal(tokenList[0], NFT_B_ID)
   })
 })
