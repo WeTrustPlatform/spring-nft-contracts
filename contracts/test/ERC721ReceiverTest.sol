a9d2d0b3fc684a79c4f13f120166a252de2dc93a
Khanh Dao <khanhdao.knd@gmail.com>
2018-09-28 14:37:38 +0700

    Fix linting issues

--- contracts/test/ERC721ReceiverTest.sol
+++ contracts/test/ERC721ReceiverTest.sol
@@ -4,12 +4,11 @@ import "../interface/ERC721TokenReceiver.sol";
 
 contract ERC721ReceiverTest is ERC721TokenReceiver {
 
-  event abitraryEvent(
-    address indexed _operator, address indexed _from, uint256 _tokenId, bytes _data);
+    event abitraryEvent(address indexed _operator, address indexed _from, uint256 _tokenId, bytes _data);
 
-  function onERC721Received(
-    address _operator, address _from, uint256 _tokenId, bytes _data) external returns (bytes4) {
-    emit abitraryEvent(_operator, _from, _tokenId, _data);
-    return 0x150b7a02;
-  }
+    function onERC721Received(
+        address _operator, address _from, uint256 _tokenId, bytes _data) external returns (bytes4) {
+        emit abitraryEvent(_operator, _from, _tokenId, _data);
+        return 0x150b7a02;
+    }
 }
--- contracts/test/EmptyContract.sol
+++ contracts/test/EmptyContract.sol
@@ -1,3 +1,4 @@
 pragma solidity ^0.4.23;
 
+/* solium-disable-next-line */
 contract EmptyContract {}
