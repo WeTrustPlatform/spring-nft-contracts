a0769ccdb48af4cc9c20ce6d33642c62f9814b4e
Khanh Dao <khanhdao.knd@gmail.com>
2018-10-03 14:17:37 +0700

    Improve transferFrom unit test (#27)

--- test/NFTokenUnitTests/transferFromUnitTest.js
+++ test/NFTokenUnitTests/transferFromUnitTest.js
@@ -1,90 +1,85 @@
-'use strict';
+'use strict'
 
+const {
+  NFT_A_ID,
+  NFT_A_TRAITS,
+  NFT_A_TYPE,
+  NON_EXISTENT_NFT_ID,
+  NON_EXISTENT_ADDRESS,
+  RAINFOREST,
+  RAINFOREST_TRUST_ID,
+  RAINFOREST_TRUST_ADDRESS,
+  RAINFOREST_TRUST_URL
+} = require('../test-data')
+
+const assert = require('chai').assert
 const springNFT = artifacts.require('SpringNFT.sol')
 const utils = require('../utils/utils')
-const consts = require('../utils/consts')
 
-let springNFTInstance;
+contract('NFToken: transferFrom Unit Test', (accounts) => {
+  const MANAGER_ADDRESS = accounts[6]
+  const SIGNER_ADDRESS = accounts[7]
+  const OWNER_ADDRESS = accounts[0]
+  const APPROVED_ADDRESS = accounts[2]
+  const TRANSFERRED_ADDRESS = accounts[3]
 
-contract('NFToken: transferFrom Unit Test', function(accounts) {
-  const recipientId = '0x1'
-  const nftHolder = accounts[0]
-  let nftId = 1;
+  let springNFTInstance
 
-  const wetrustAddress = accounts[7];
-  const managerAddress = accounts[6];
-  beforeEach(async function() {
-    springNFTInstance = await springNFT.new(wetrustAddress, managerAddress);
+  beforeEach(async () => {
+    springNFTInstance = await springNFT.new(SIGNER_ADDRESS, MANAGER_ADDRESS)
 
-    await springNFTInstance.addRecipient(recipientId, 'name', 'url', '0x0', {from: wetrustAddress})
-    await springNFTInstance.createNFT(nftId, nftHolder, recipientId, '0x01', '0x01', {from: wetrustAddress})
-  });
+    await springNFTInstance.addRecipient(
+      RAINFOREST_TRUST_ID, RAINFOREST, RAINFOREST_TRUST_URL, RAINFOREST_TRUST_ADDRESS,
+      { from: SIGNER_ADDRESS })
+    await springNFTInstance.createNFT(
+      NFT_A_ID, OWNER_ADDRESS, RAINFOREST_TRUST_ID, NFT_A_TRAITS, NFT_A_TYPE,
+      { from: SIGNER_ADDRESS })
+  })
 
-  it('checks that proper values were updated', async function() {
-    let totalSupply = await springNFTInstance.totalSupply.call()
-    assert.equal(totalSupply, 1)
+  it('checks that transferred address is the new owner of transferred token', async () => {
+    await springNFTInstance.transferFrom(OWNER_ADDRESS, TRANSFERRED_ADDRESS, NFT_A_ID)
 
-    const newNFTHolder = accounts[3]
-    let owner = await springNFTInstance.ownerOf.call(nftId)
-    assert.equal(owner, nftHolder)
+    assert.equal(await springNFTInstance.ownerOf.call(NFT_A_ID), TRANSFERRED_ADDRESS)
+  })
 
-    await springNFTInstance.transferFrom(nftHolder, newNFTHolder, nftId)
+  it('checks that approved address is removed after token is transferred', async () => {
+    await springNFTInstance.approve(APPROVED_ADDRESS, NFT_A_ID)
 
-    owner = await springNFTInstance.ownerOf.call(nftId)
-    assert.equal(owner, newNFTHolder)
+    await springNFTInstance.transferFrom(
+      OWNER_ADDRESS, TRANSFERRED_ADDRESS, NFT_A_ID, { from: OWNER_ADDRESS })
 
-    totalSupply = await springNFTInstance.totalSupply.call()
-    assert.equal(totalSupply, 1)
-  });
+    assert.equal(await springNFTInstance.ownerOf.call(NFT_A_ID), TRANSFERRED_ADDRESS)
+    assert.equal(await springNFTInstance.getApproved.call(NFT_A_ID), NON_EXISTENT_ADDRESS)
+  })
 
-  it('checks that approval is removed when token is transferred', async function() {
-    const newNFTHolder = accounts[3]
-    let owner = await springNFTInstance.ownerOf.call(nftId)
-    assert.equal(owner, nftHolder)
+  it('throw if transferring to zero address', async () => {
+    await utils.assertRevert(
+      springNFTInstance.transferFrom(
+        OWNER_ADDRESS, NON_EXISTENT_ADDRESS, NFT_A_ID, { from: OWNER_ADDRESS }))
+  })
 
-    const approvedAddress = accounts[2]
-    await springNFTInstance.approve(approvedAddress, nftId)
+  it('throw if msg.sender is not owner/approved/operator of the token', async () => {
+    await utils.assertRevert(
+      springNFTInstance.transferFrom(
+        OWNER_ADDRESS, TRANSFERRED_ADDRESS, NFT_A_ID, { from: SIGNER_ADDRESS }))
+  })
 
-    await springNFTInstance.transferFrom(nftHolder, newNFTHolder, nftId)
+  it('throw if transferring a non-existent token', async () => {
+    await utils.assertRevert(
+      springNFTInstance.transferFrom(
+        OWNER_ADDRESS, TRANSFERRED_ADDRESS, NON_EXISTENT_NFT_ID, { from: OWNER_ADDRESS }))
+  })
 
-    owner = await springNFTInstance.ownerOf.call(nftId)
-    assert.equal(owner, newNFTHolder)
+  it('throw if "from" address is not token owner address', async () => {
+    await utils.assertRevert(
+      springNFTInstance.transferFrom(
+        TRANSFERRED_ADDRESS, OWNER_ADDRESS, NFT_A_ID, { from: OWNER_ADDRESS }))
+  })
 
-    const approved = await springNFTInstance.getApproved.call(nftId)
-    assert.equal(approved, consts.ZERO_ADDRESS)
-  });
+  it('checks that transfer event is emitted after transfer succeeds', async () => {
+    const res = await springNFTInstance
+      .transferFrom(OWNER_ADDRESS, TRANSFERRED_ADDRESS, NFT_A_ID, { from: OWNER_ADDRESS })
 
-  it('throw if "to" is zero address', async function() {
-    const newNFTHolder = accounts[3]
-
-    await utils.assertRevert(springNFTInstance.transferFrom(nftHolder, '0x0', nftId))
-    await springNFTInstance.transferFrom(nftHolder, newNFTHolder, nftId)
-  });
-
-  it('throw if msg.sender does not have permission to transfer', async function() {
-    const newNFTHolder = accounts[3]
-
-    await utils.assertRevert(springNFTInstance.transferFrom(nftHolder, newNFTHolder, nftId, {from: wetrustAddress}))
-    await springNFTInstance.transferFrom(nftHolder, newNFTHolder, nftId)
-  });
-
-  it('throw if token does not have an owner', async function() {
-    const newNFTHolder = accounts[3]
-
-    await utils.assertRevert(springNFTInstance.transferFrom(nftHolder, newNFTHolder, nftId + 1))
-    await springNFTInstance.transferFrom(nftHolder, newNFTHolder, nftId)
-  });
-
-  it('throw if from does not equal owner address', async function() {
-    const newNFTHolder = accounts[3]
-
-    await utils.assertRevert(springNFTInstance.transferFrom(newNFTHolder, nftHolder, nftId))
-    await springNFTInstance.transferFrom(nftHolder, newNFTHolder, nftId)
-  });
-
-  it('checks that transfer event is emitted', async function() {
-    const newNFTHolder = accounts[3]
-    const res = await springNFTInstance.transferFrom(nftHolder, newNFTHolder, nftId)
     assert.equal(res.logs[0].event, 'Transfer')
-  });
-});
+  })
+})
